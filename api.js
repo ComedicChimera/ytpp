5977f4e59ea8766f70a34c74b5854ac646d2cf6b
ComedicChimera <forlornsisu@gmail.com>
2018-03-20 01:16:52 -0500

    initial commit

---
+++ api.js
@@ -0,0 +1,3 @@
+module.exports.testRemote = () => {
+  console.log('test');
+}
---
+++ main.js
@@ -0,0 +1,46 @@
+// includes
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+
+// window variable
+let win;
+
+// create browser window
+function createWindow() {
+  win = new BrowserWindow({width: 960, height: 720});
+
+  // remove top menu
+  win.setMenu(null);
+
+  // load index.html
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // when window is closed, close window
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+// start app when ready
+app.on('ready', createWindow);
+
+// when the windows are all closed
+app.on('window-all-closed', () => {
+  // do not close if on MacOS
+  if (process.platform !== 'darwin')
+    app.quit();
+});
+
+// MacOS doc reopen
+app.on('activate', () => {
+  if (win === null)
+    createWindow();
+});
