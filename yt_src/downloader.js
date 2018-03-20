142ea73b2087a81ee80b901fd5f032a831e1306c
ComedicChimera <forlornsisu@gmail.com>
2018-03-20 10:22:03 -0500

    added yt search

--- yt_src/downloader.js
+++ yt_src/downloader.js
@@ -1,5 +1,14 @@
 const ytdl = require('ytdl-core');
+const {win} = require('../main.js');
+const ytSearch = require('youtube-search');
 
+
+function setRemoteStatus(message, progress) {
+  win.webContents.send('set-status', {
+    message: message,
+    progress: progress
+  });
+}
 
 module.exports.download = (query, path, format) => {
   let stream;
@@ -7,13 +16,19 @@ module.exports.download = (query, path, format) => {
     stream = getStream(query, format);
   }
   else {
-    stream = getStream(getUrl(query), format);
+    getUrl(query).then(result => {
+
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 }
 
 let formatRemaps = {
   'mp3': 'm4a',
-  'avi': 'mp4',
+  'avi': 'mp4',1
   'mov': 'mp4',
   'wav': 'm4a',
 }
@@ -24,6 +39,11 @@ function getStream(url, format) {
   return ytdl(url, {filter: (format) => format.container === format});
 }
 
-function getUrl(query) {
-  
+async getUrl(query) {
+  return new Promise((resolve, reject) => {
+    youtube_search(query, {maxResults: 1, key: "--"}, (err, results) => {
+      if (err) reject(err);
+      else resolve(results);
+    });
+  });
 }
